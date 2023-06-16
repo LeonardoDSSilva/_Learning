c5b168888709ed893cb879b26018295a13b86bb0
Leonardo Silva <ldss1@outlook.com>
2023-06-15 23:27:42 -0300

    Comentario

---
+++ JavaScript/Serie para Iniciantes em JavaScript/aula06.js
@@ -0,0 +1,22 @@
+// Comentarios de uma linha
+
+/*
+
+Comentarios de multiplas linhas
+
+*/
+
+var x = 4; // Comentario de uma linha
+var y = 5; /* Comentario de multiplas linhas */
+
+
+console.log(x + y);
+
+
+/* const cachorro = {
+	nome: 'Bob',
+	rara: 'Pitbull',
+	idade: 5
+};
+
+console.log(cachorro.nome); */
