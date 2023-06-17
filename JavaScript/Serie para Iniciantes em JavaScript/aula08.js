d2b85e9bb09a3401d8cd512bd73b1b7e7e8b1d56
Leonardo Silva <ldss1@outlook.com>
2023-06-16 23:45:39 -0300

    Variavel

---
+++ JavaScript/Serie para Iniciantes em JavaScript/aula08.js
@@ -0,0 +1,34 @@
+// Variavel
+
+// Exemplo do uso do var:
+
+var nome = "Rafael";
+var nome = "Rafael2";
+
+console.log(nome);
+
+
+// Exemplo do uso do let:
+
+let nome2 = "Rafael";
+nome2 = "Rafael2";
+
+console.log(nome2);
+
+
+// Exemplo do uso do const:
+
+const nome3 = "Rafael";
+
+console.log(nome3);
+
+
+// Exemplo do uso do const com objeto:
+
+const cachorro = {
+	nome: 'Bob',
+	raca: 'Pitbull',
+	idade: 5
+};
+
+console.log(cachorro.nome);
