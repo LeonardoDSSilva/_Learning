6f91b4bcbd801127e6552068d9c4644d6eda4f5b
Leonardo Silva <ldss1@outlook.com>
2023-06-17 23:56:17 -0300

    Formato

---
+++ JavaScript/Serie para Iniciantes em JavaScript/aula10.js
@@ -0,0 +1,15 @@
+let str1 = 'Ola';
+let str2 = 'Mundo';
+
+let str3 = str1 + ' ' + str2 + '!';
+
+console.log(`${str1} ${str2}!`);
+
+console.log(str1.concat(str1, str2));
+
+let num1 = 1;
+let num2 = '1';
+
+console.log(num1 + num2);
+
+console.log(num1 + Number(num2));
