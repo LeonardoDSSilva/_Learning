b17476a953cf1d156a833297788eaf74132df146
Leonardo Silva <ldss1@outlook.com>
2023-06-21 23:22:03 -0300

    Aula28: Array

---
+++ JavaScript/Serie para Iniciantes em JavaScript/aula28.js
@@ -0,0 +1,15 @@
+// Criando um Array
+let animais = ['Cachorro', 'Gato', 'Coelho', 'Peixe', 'Cavalo'];
+console.log(animais);
+
+let idiomas = new Array('Inglês', 'Português', 'Espanhol');
+console.log(idiomas);
+
+// Acessando um item do Array
+console.log(animais[0]);
+
+// Acessando o tamanho do Array
+console.log(animais.length);
+
+// Acessando o último item do Array
+console.log(animais[animais.length - 1]);
