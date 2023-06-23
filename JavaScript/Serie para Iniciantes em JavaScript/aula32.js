c4e7df8b1209fe2785896df3cd19bcc9fa6a9589
Leonardo Silva <ldss1@outlook.com>
2023-06-22 23:33:56 -0300

    Aula32: Metodos Array

---
+++ JavaScript/Serie para Iniciantes em JavaScript/aula32.js
@@ -0,0 +1,20 @@
+const array1 = [1, 2, 3, 4, 5, 6];
+console.log(array1.push(7));
+console.log(array1);
+
+
+console.log(array1.pop());
+console.log(array1);
+
+
+const array2 = ['A', true, 2];
+console.log(array2.unshift('B'));
+console.log(array2);
+
+console.log(array2.shift());
+console.log(array2);
+
+let array3 = array1.concat(array2);
+console.log(array3);
+
+
