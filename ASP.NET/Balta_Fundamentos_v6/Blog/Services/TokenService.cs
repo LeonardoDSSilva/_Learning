b2176b00eb16a02fff5ffcbcc786b3aece827018
Leonardo Silva <ldss1@outlook.com>
2023-08-29 13:05:07 -0300

    Token

---
+++ ASP.NET/Balta_Fundamentos_v6/Blog/Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Balta_Fundamentos.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.Name, ""), //User.Identity.Name
+                new (ClaimTypes.Role, "") //User.IsInRole
+            }),
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
