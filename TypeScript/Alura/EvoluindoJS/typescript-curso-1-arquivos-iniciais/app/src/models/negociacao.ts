3c50a872cff0fd4cc29df621c178a1706efadcc7
Leonardo Silva <ldss1@outlook.com>
2023-12-16 21:36:57 -0300

    Refactor Negociacao class to extend Imprimivel
    This change refactors Negociacao to extend Imprimivel for code reusability and adhering to the Open/Closed Principle. The constructor is simplified, and redundant getter methods are removed. Volume and data properties remain accessible. This design improvement enhances maintainability and promotes code consolidation.

--- TypeScript/Alura/EvoluindoJS/typescript-curso-1-arquivos-iniciais/app/src/models/negociacao.ts
+++ TypeScript/Alura/EvoluindoJS/typescript-curso-1-arquivos-iniciais/app/src/models/negociacao.ts
@@ -1,11 +1,32 @@
-export class Negociacao{
+import { Imprimivel } from "../utils/imprimivel.js";
 
+export class Negociacao extends Imprimivel{
+	
 	// Pode cria construtor ja com os atributos privados e com o tipo deles 
 	constructor(
 		private _data : Date,
 		public readonly quantidade : number,
 		public readonly valor : number
-	) {}
+	) {
+		super();
+	}
+
+	public static criarDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+		const data = new Date(dataString.replace('-', ','));
+		const quantidade = parseInt(quantidadeString);
+		const valor = parseFloat(valorString);
+
+		return new Negociacao(data, quantidade, valor);
+	}
+	
+	public paraTexto(): string {
+		return `
+			Data: ${new Intl.DateTimeFormat().format(this.data)}
+			Quantidade: ${this.quantidade}
+			Valor: ${this.valor}
+			Volume: ${this.volume}
+		`
+	}
 
 	get data() : Date {
 		const data = new Date(this._data.getTime());
@@ -16,19 +37,4 @@ export class Negociacao{
 		return this.quantidade * this.valor;
 	}
 
-	public static criarDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-		const data = new Date(dataString.replace('-', ','));
-		const quantidade = parseInt(quantidadeString);
-		const valor = parseFloat(valorString);
-
-		return new Negociacao(data, quantidade, valor);
-	}
-	public paraTexto(): string {
-		return `
-			Data: ${new Intl.DateTimeFormat().format(this.data)}
-			Quantidade: ${this.quantidade}
-			Valor: ${this.valor}
-			Volume: ${this.volume}
-		`
-	}
 }
