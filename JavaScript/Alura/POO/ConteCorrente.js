a653f5a93d7fc1c299f9c75ad492f3ce544c5cac
Leonardo Silva <ldss1@outlook.com>
2024-01-04 23:08:24 -0300

    Add ContaCorrente class with methods for withdrawing, depositing, and displaying balance

---
+++ JavaScript/Alura/POO/ConteCorrente.js
@@ -0,0 +1,23 @@
+class ContaCorrente {
+	agencia;
+	#saldo = 0
+
+	constructor(agencia) {
+		this.agencia = agencia;
+	}
+
+	sacar(valor) {
+		if (this.#saldo >= valor) {
+			this.#saldo -= valor;
+		}
+	}
+
+	depositar(valor) {
+		if (valor <= 0) return;
+		this.#saldo += valor;
+	}
+
+	exibirSaldo() {
+		console.log(this.#saldo);
+	}
+}
