a0a5ae1a76a794c4e05d167e943ce82861e7a7fa
Leonardo Silva <ldss1@outlook.com>
2024-01-06 20:57:27 -0300

    Add Cliente import and update constructor in ContaCorrente class

--- JavaScript/Alura/POO/ConteCorrente.js
+++ JavaScript/Alura/POO/ConteCorrente.js
@@ -1,9 +1,14 @@
+import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
+	static numeroDeContas = 0;
 	agencia;
+	_cliente;
 	#saldo = 0
 
-	constructor(agencia) {
+	constructor(cliente, agencia) {
 		this.agencia = agencia;
+		this._cliente = cliente;
+		this.numeroDeContas += 1;
 	}
 
 	sacar(valor) {
@@ -17,7 +22,26 @@ export class ContaCorrente {
 		this.#saldo += valor;
 	}
 
+	tranferir(valor, conta) {
+		const valorSacado = this.sacar(valor);
+		conta.depositar(valorSacado);
+	}
+	
 	exibirSaldo() {
 		console.log(this.#saldo);
+	}
+
+	set cliente(novoValor) {
+		if (novoValor instanceof Cliente) {
+			this._cliente = novoValor;
+		}
+	}
+
+	get cliente() {
+		return this._cliente;
+	}
+
+	get saldo() {
+		return this.#saldo;
 	}
 }
