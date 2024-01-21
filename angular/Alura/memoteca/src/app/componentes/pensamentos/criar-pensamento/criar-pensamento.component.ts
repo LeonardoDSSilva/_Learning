a938bbdf6255025ed14dab8aa45f93091797d637
Leonardo Silva <ldss1@outlook.com>
2024-01-20 23:54:49 -0300

    Add error messages and disabled button style

--- angular/Alura/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular/Alura/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms'; // <-- Importe o FormGroup e o FormBuilder
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // <-- Importe o FormGroup e o FormBuilder
 
 import { PensamentoService } from '../pensamento.service';
 
@@ -20,22 +20,37 @@ export class CriarPensamentoComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.formulario = this.formBuilder.group({
-			conteudo: [null],
-			autoria: [null],
+			conteudo: [null, Validators.compose([
+				Validators.required,
+				Validators.pattern(/(.|\s)*\S(.|\s)*/),
+			])],
+			autoria: [null, Validators.compose([
+				Validators.required,
+				Validators.pattern(/(.|\s)*\S(.|\s)*/),
+				Validators.minLength(3),
+			])],
 			modelo: ['modelo1']
 		});
 	}
 
 	criarPensamento(): void {
-		this.pensamentoService.criar(this.formulario.value).subscribe(
-			() => {
-				this.router.navigate(['/listarPensamento']);
-			}
-		);
+		if(this.formulario.valid) {
+			this.pensamentoService.criar(this.formulario.value).subscribe(
+				() => this.router.navigate(['/listarPensamento'])
+			);
+		}
 	}
 
 	cancelar(): void {
 		this.router.navigate(['/listarPensamento']);
+	}
+
+	habilitarBotao(): string {
+		if(this.formulario.valid) {
+			return 'botao';
+		} else {
+			return 'botao__desabilitado';
+		}
 	}
 
 }
