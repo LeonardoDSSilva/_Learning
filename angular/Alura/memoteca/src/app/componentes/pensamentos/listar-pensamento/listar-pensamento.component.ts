0e54ed9de14dabaa740a91f8cf5dd4d7ac1ed831
Leonardo Silva <ldss1@outlook.com>
2024-01-21 19:22:58 -0300

    Add search functionality and load more button

--- angular/Alura/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular/Alura/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,13 +10,35 @@ import { PensamentoService } from '../pensamento.service';
 export class ListarPensamentoComponent implements OnInit {
 
 	listaPensamentos: Pensamento[] = [];
+	haMaisPensamentos: boolean = true;
+	filtro: string = '';
+	paginaAtual = 1;
 
 	constructor(private pensamentoService: PensamentoService) { }
 
 	ngOnInit(): void {
-		this.pensamentoService.listar().subscribe(
+		this.pensamentoService.listar(this.paginaAtual, this.filtro).subscribe(
 			pensamentos => this.listaPensamentos = pensamentos
 		);
 	}
 
+	carregarMaisPensamentos(): void {
+		this.pensamentoService.listar(++this.paginaAtual, this.filtro).subscribe(
+			pensamentos => {
+				if (pensamentos.length) {
+					this.listaPensamentos.push(...pensamentos);
+				} else {
+					this.haMaisPensamentos = false;
+				}
+			}
+		);
+	}
+
+	pesquisarPensamentos() {
+		this.paginaAtual = 1;
+		this.haMaisPensamentos = true;
+
+		this.pensamentoService.listar(this.paginaAtual, this.filtro)
+			.subscribe( pensamentos => this.listaPensamentos = pensamentos );
+	}
 }
